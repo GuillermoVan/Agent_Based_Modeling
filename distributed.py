195a674490aa1be8279f2a10acec401d91669063
Lisa Scholtens <lisa.scholtens@gmail.com>
2024-03-12 17:07:12 +0100

    Toevoegingen conflict en grote loop, toevoegen paths aan results in run_experiments

--- distributed.py
+++ distributed.py
@@ -66,16 +66,18 @@ class DistributedPlanningSolver(object):
 
         #while not all agents have reached their target
         while len(arrived) < len(self.distributed_agents):
-            print(arrived)
-            for agent_1 in self.distributed_agents:
-                if agent_1 not in arrived:
+            change = True
+            while change == True:
+            # print(arrived)
+                for agent_1 in self.distributed_agents:
+                    # if agent_1 not in arrived:
                     if time >= len(agent_1.path):
                         agent_1.path.append(agent_1.path[-1])
 
                     scope_map = self.define_scope(paths, time, agent_1, scope_rad=2)
                     agents_in_scope = self.detect_agent_in_scope(agent_1, scope_map, time)
                     for agent_2 in agents_in_scope:
-                        constraints = self.conflict(agent_1, agent_2, time, constraints)
+                        constraints, change = self.conflict(agent_1, agent_2, time, constraints)
 
                     if agent_1.path[time] == agent_1.goal:
                         arrived.append(agent_1)
@@ -83,13 +85,13 @@ class DistributedPlanningSolver(object):
                     agent_1.start = agent_1.path[time]
 
 
-            time += 1
+                time += 1
 
         result = []
         for agent in self.distributed_agents:
             result.append(agent.path)
 
-        print(result)
+        # print(result)
         return result  # Hint: this should be the final result of the distributed planning (visualization is done after planning)
 
     def define_scope(self, result, timestep, agentID1, scope_rad=2):
@@ -110,21 +112,22 @@ class DistributedPlanningSolver(object):
         return scope_map
 
     def conflict(self, agent_1, agent_2, time, constraints):
+        change = False
         for i in range(1,4): # Communicate 3 time steps ahead
             avoidance = False   # Check for avoidance for all 3 time steps
             timestep = time + i  # Timestep which is checked
 
             while avoidance == False:   # Only continue when collision is avoided
                 """ Let agent stay at its final location after path is finished."""
-                while timestep + 1 >= len(agent_1.path):
+                while timestep + 2 >= len(agent_1.path):
                     agent_1.path.append(agent_1.path[-1])
 
-                while timestep + 1 >= len(agent_2.path):
+                while timestep + 2 >= len(agent_2.path):
                     agent_2.path.append(agent_2.path[-1])
 
                 """ Check if collision occurs."""
-                if agent_1.path[timestep] == agent_2.path[timestep]:
-                    print(timestep)
+                if agent_1.path[timestep] == agent_2.path[timestep] or (agent_1.path[timestep] == agent_2.path[timestep-1] and agent_1.path[timestep-1] == agent_2.path[timestep]):
+
                     """ Create alternative paths."""
                     constraint_temp_1 = []
                     constraint_temp_2 = []
@@ -146,137 +149,39 @@ class DistributedPlanningSolver(object):
                         constraint_temp_1.append({'positive': False,
                                                 'negative': True,
                                                 'agent': agent_1.id,
-                                                'loc': [agent_2.path[j+time], agent_2.path[j-1+time]],
+                                                'loc': [agent_2.path[timestep-1], agent_2.path[timestep]],
                                                 'timestep': j})
 
                         constraint_temp_2.append({'positive': False,
                                                 'negative': True,
                                                 'agent': agent_2.id,
-                                                'loc': [agent_1.path[j+time], agent_1.path[j-1+time]],
+                                                'loc': [agent_1.path[timestep-1], agent_1.path[timestep]],
                                                 'timestep': j})
 
                     path_1 = agent_1.find_solution(constraints=constraint_temp_1)
                     path_2 = agent_2.find_solution(constraints=constraint_temp_2)
-
-                    if len(path_1) >= len(path_2): # Agent with longest detour receives priority
+                    a = np.random.normal(0,1)
+                    if a >= 0.5: # Agent with longest detour receives priority
+                        print(agent_2.path)
                         agent_2.path = agent_2.path[:time] + path_2
-                        constraints.append(constraint_temp_2)
+                        constraints = constraint_temp_2
+                        print('CHANGED PATH 1>2', agent_1.path, agent_2.path)
+                        change = True
                     else:
+                        print(agent_1.path)
                         agent_1.path = agent_1.path[:time] + path_1
-                        constraints.append(constraint_temp_1)
+                        constraints = constraint_temp_1
+                        change = True
+                        print('CHANGED PATH 2>1', agent_1.path, agent_2.path)
+
+                    print('CHANGED:', agent_1.id, agent_2.id)
 
 
                 else:   # No collision occurs at timestep
                     avoidance = True
 
-        return constraints
-    # def conflict(self, agent_1, agent_2, time, constraints):
-    #     for i in range(3):  # Communicate 3 time steps
-    #         avoidance = False
-    #         timestep = time + (i + 1)
-    #
-    #         while avoidance == False:  # Only continue when collision is avoided
-    #
-    #             if timestep >= len(agent_1.path):
-    #                 agent_1.path.append(agent_1.path[-1])
-    #                 curr_loc_1 = agent_1.path[-1]
-    #             else:
-    #                 curr_loc_1 = agent_1.path[timestep]
-    #
-    #             if timestep >= len(agent_2.path):
-    #                 agent_2.path.append(agent_2.path[-1])
-    #                 curr_loc_2 = agent_2.path[-1]
-    #             else:
-    #                 curr_loc_2 = agent_2.path[timestep]
-    #
-    #             if curr_loc_1 == curr_loc_2:  # Collision at timestep
-    #                 print("PATHS", agent_1.path, agent_2.path)
-    #                 """ Construct alternative (temporary) paths for the colliding agents"""
-    #                 constraint_temp_1 = []
-    #                 constraint_temp_2 = []
-    #                 time_temp = 0
-    #                 for loc in agent_1.path:
-    #                     constraint_temp_2.append({'positive': False,
-    #                                               'negative': True,
-    #                                               'agent': agent_2.id,
-    #                                               'loc': loc,
-    #                                               'timestep': time_temp})
-    #                     time_temp += 1
-    #
-    #                 time_temp = 0
-    #                 for loc in agent_2.path:
-    #                     constraint_temp_1.append({'positive': False,
-    #                                               'negative': True,
-    #                                               'agent': agent_1.id,
-    #                                               'loc': loc,
-    #                                               'timestep': time_temp})
-    #                     time_temp += 1
-    #
-    #                 # constraint_temp.append({'positive': False,
-    #                 #                         'negative': True,
-    #                 #                         'agent': agent_1.id,
-    #                 #                         'loc': [curr_loc_1],
-    #                 #                         'timestep': timestep - time
-    #                 #                         })
-    #                 start_1 = agent_1.start
-    #                 start_2 = agent_2.start
-    #
-    #                 path_1 = agent_1.find_solution(constraints=constraints + constraint_temp_1)
-    #                 path_2 = agent_2.find_solution(constraints=constraints + constraint_temp_2)
-    #
-    #                 # constraint_temp.append({'positive': False,
-    #                 #                         'negative': True,
-    #                 #                         'agent': agent_2.id,
-    #                 #                         'loc': [curr_loc_2],
-    #                 #                         'timestep': timestep - time
-    #                 #                         })
-    #                 #
-    #
-    #                 #
-    #                 # agent_1.start = agent_1.path[timestep-1]
-    #                 # path_1 = agent_1.find_solution(constraints=constraints + constraint_temp)
-    #
-    #                 # for place in range(len(path_1)-1):
-    #                 #     constraint_temp.append({'positive': False,
-    #                 #                             'negative': True,
-    #                 #                             'agent': agent_2,
-    #                 #                             'loc':[path_1[place], path_1[place+1]],
-    #                 #                             'timestep': place})
-    #                 print(path_1)
-    #                 # agent_2.start = agent_2.path[timestep-1]
-    #                 # path_2 = agent_2.find_solution(constraints=constraints + constraint_temp)
-    #                 print(path_2)
-    #                 """ Agent with the longest detour gets to keep its original path."""
-    #                 #
-    #                 agent_1.start = start_1
-    #                 agent_2.start = start_2
-    #
-    #                 a = np.random.normal()
-    #
-    #                 if a >= 0.5:
-    #
-    #                     agent_2.path = agent_2.path[:timestep] + path_2
-    #
-    #                     """ The changed path cannot be changed back to include the collision point at the same timestep."""
-    #                     # ADD CONSTRAINT FOR SWITCHING POSITIONS
-    #                     constraints.append({'positive': False,
-    #                                         'negative': True,
-    #                                         'agent': agent_2.id,
-    #                                         'loc': [curr_loc_1],
-    #                                         'timestep': timestep
-    #                                         })
-    #                 else:
-    #                     agent_1.path = agent_1.path[:timestep] + path_1
-    #                     constraints.append({'positive': False,
-    #                                         'negative': True,
-    #                                         'agent': agent_1.id,
-    #                                         'loc': [curr_loc_2],
-    #                                         'timestep': timestep
-    #                                         })
-    #             else:
-    #                 avoidance = True
-    #
-    #     return constraints
+        return constraints, change
+
 
 # DETECT AGENT
 # Function 2: input = map of 0s and 1s, current location all agents -> if agent detected, then output = detected agent's object
